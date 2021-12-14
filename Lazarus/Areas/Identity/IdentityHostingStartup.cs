e8f4d6f04b80e1b7295d1f485cfd9d85f9948515
Muhammad Mubashir <techsolutionsofficia1@gmail.com>
2021-12-14 22:31:30 +0500

    Add project files.

---
+++ Lazarus/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Lazarus.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Lazarus.Areas.Identity.IdentityHostingStartup))]
+namespace Lazarus.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
